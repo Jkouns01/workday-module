694f457b936088943c253fb0b0ffdb31b4677428
[Jacob Kouns] <[jacob.kouns01@gmail.com]>
2023-01-24 12:55:31 -0600

    fixing javascript

--- script.js
+++ script.js
@@ -1,42 +1,45 @@
 $(document).ready(function () {
-    //display current day & time.
+    //variables for each hour section of html
+    $("#hour_9 .description").val(localStorage.getItem("hour_9"));
+    $("#hour_10 .description").val(localStorage.getItem("hour_10"));
+    $("#hour_11 .description").val(localStorage.getItem("hour_11"));
+    $("#hour_12 .description").val(localStorage.getItem("hour_12"));
+    $("#hour_13 .description").val(localStorage.getItem("hour_13"));
+    $("#hour_14 .description").val(localStorage.getItem("hour_14"));
+    $("#hour_15 .description").val(localStorage.getItem("hour_15"));
+    $("#hour_16 .description").val(localStorage.getItem("hour_16"));
+    $("#hour_17 .description").val(localStorage.getItem("hour_17"));
+
+//time display on the top of screen
     $("#time-display").text(dayjs().format("MMMM D YYYY"));
 
-    
-    $(".saveBtn").on("click", function () {
-        console.log(this);
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+    //TODO: need to fix save button to save to JSON 
+    $(".saveBtn").on("click", planSaveHandler);
 
-     
-        localStorage.setItem(time, text);
-    })
-    $("#hour9 .description").val(localStorage.getItem("hour_9"));
-    $("#hour10 .description").val(localStorage.getItem("hour_10"));
-    $("#hour11 .description").val(localStorage.getItem("hour_11"));
-    $("#hour12 .description").val(localStorage.getItem("hour_12"));
-    $("#hour13 .description").val(localStorage.getItem("hour_13"));
-    $("#hour14 .description").val(localStorage.getItem("hour_14"));
-    $("#hour15 .description").val(localStorage.getItem("hour_15"));
-    $("#hour16 .description").val(localStorage.getItem("hour_16"));
-    $("#hour17 .description").val(localStorage.getItem("hour_17"));
+    function planSaveHandler (){
+        let plan = $(this).siblings(".description").val();
+        console.log("plan= ",plan);
+        let planTime = $(this).parent().attr("id");
+        console.log("planTime= ",planTime);
 
+        localStorage.setItem(planTime, plan);
+    }
 
     function hourFunction() {
-        var currentHour = dayjs().hour();
+        var thisHour = dayjs().hour();
 
        
         $(".time-block").each(function () {
-            var timeSpot = parseInt($(this).attr("id").split("hour")[1]);
-            console.log( timeSpot, currentHour)
+            var timeSpot = parseInt($(this).attr("id").split("_")[1]);
+            console.log( timeSpot, thisHour)
 
     
-            if (timeSpot < currentHour) {
+            if (timeSpot < thisHour) {
                 $(this).addClass("past");
                 $(this).removeClass("future");
                 $(this).removeClass("present");
             }
-            else if (timeSpot === currentHour) {
+            else if (timeSpot === thisHour) {
                 $(this).removeClass("past");
                 $(this).addClass("present");
                 $(this).removeClass("future");
